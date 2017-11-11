56032b8420cac942eaf6c85a2b172088397e1055
aklam <aklam@princeton.edu>
2017-11-11 16:53:22 -0500

    update goodmorning

--- GoodMorning.sh
+++ GoodMorning.sh
@@ -1,2 +1,10 @@
 #download data and decompress it
-wget https://s3.us-east-2.amazonaws.com/iw07alexlamfacesdataset/haha.jpg
+wget https://s3.us-east-2.amazonaws.com/iw07alexlamfacesdataset/data.zip
+unzip data.zip
+rm data.zip
+
+wget https://s3.us-east-2.amazonaws.com/iw07alexlamfacesdataset/faces96.zip
+unzip faces96.zip
+rm faces96.zip
+mv faces96 EssexFaces
+

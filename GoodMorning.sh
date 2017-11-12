a35e1ee285e2ffb8a9a83b74f5dce9b982fbf161
aklam <aklam@princeton.edu>
2017-11-11 21:55:04 -0500

    minor fix

--- GoodMorning.sh
+++ GoodMorning.sh
@@ -1,5 +1,5 @@
 #download data and decompress it
-#wget https://s3.us-east-2.amazonaws.com/iw07alexlamfacesdataset/data.zip
+wget https://s3.us-east-2.amazonaws.com/iw07alexlamfacesdataset/data.zip
 unzip data.zip
 rm data.zip
 

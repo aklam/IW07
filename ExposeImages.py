4a7b996bcd091a5510dddde8356b98af3fc97fee
aklam <aklam@princeton.edu>
2017-11-13 09:21:45 -0500

    add min function to pixel multiplication

--- ExposeImages.py
+++ ExposeImages.py
@@ -16,18 +16,36 @@ def exposeImg(file):
     exposeLVL  = 1.6
     exposeLVL2 = 2
 
+    #try to recover image by just multiplying
+    rec = Image.new("RGB",(image.width, image.height))
+
     for w in range(0, image.width):
         for h in range(0, image.height):
             r,g,b = image.getpixel((w,h))
+
             r2 = math.ceil(r * exposeLVL)
             g2 = math.ceil(g * exposeLVL)
             b2 = math.ceil(b * exposeLVL)
+
+            if r2 > 255:
+                r2 = 255
+            if g2 > 255:
+                g2 = 255
+            if b2 > 255:
+                b2 = 255
             expose.putpixel((w, h), (r2, g2, b2))
 
             r3 = math.ceil(r * exposeLVL2)
             g3 = math.ceil(g * exposeLVL2)
             b3 = math.ceil(b * exposeLVL2)
+            if r3 > 255: 
+                r3 = 255
+            if g3 > 255:
+                g3 = 255
+            if b3 > 255:
+                b3 = 255
             expose2.putpixel((w, h), (r3, g3, b3))
+
 
     newName = fileName + "_explvl_" + str(exposeLVL) + "." + fileExt
     newName2 = fileName + "_explvl_" + str(exposeLVL2) + "." + fileExt
@@ -37,6 +55,9 @@ def exposeImg(file):
     expose.save(wd + "/exposelvl1/" + newName)
     expose2.save(wd + "/exposelvl2/" + newName2)
 
+    
+
+
 #def gammaCor():
 #    obama = Image.open('Obama.jpg')
 #    image = img_as_float(data.moon())
@@ -44,17 +65,19 @@ def exposeImg(file):
 #    print (image.mean() > gamma_corrected.mean())
 
 #exposeImg('Obama.jpg')
-os.chdir(os.getcwd() + "/groundTruth")
-currentDir = os.getcwd()
-filelist=os.listdir(currentDir)
-for fichier in filelist[:]:
-    if not(fichier.endswith(".png")):
-        filelist.remove(fichier)
+def allPics():
+    os.chdir(os.getcwd() + "/groundTruth")
+    currentDir = os.getcwd()
+    filelist=os.listdir(currentDir)
+    for fichier in filelist[:]:
+        if not(fichier.endswith(".png")):
+            filelist.remove(fichier)
+    
+    savewd = os.getcwd()
+    for f in filelist[:]:
+        os.chdir(savewd)
+        print("doing " + f)
+        exposeImg(f)
 
-savewd = os.getcwd()
-for f in filelist[:]:
-    os.chdir(savewd)
-    print("doing " + f)
-    exposeImg(f)
 
-
+allPics()

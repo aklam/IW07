6e450fdbde4920c633246ec483f401ca64323a5b
aklam <aklam@princeton.edu>
2017-11-20 10:42:15 -0500

    break down GoodMorning

--- GoodMorning.sh
+++ GoodMorning.sh
@@ -3,39 +3,9 @@ cp -r ec2setup ~
 
 #download data and decompress it
 wget https://s3.us-east-2.amazonaws.com/iw07alexlamfacesdataset/data.zip
-unzip data.zip
-rm data.zip
 
-
-#copy images from african directory
-cd data/african/imgs
-cp *.png ../../
-cd ../../
-
-#copy images from flickr directory
-cd flickr2/imgs
-cp *.png ../../
-cd ../../
-
-##copy images from martin
-cd martin/imgs
-cp *.png ../../
-cd ../../
-
-echo "done copying"
-rm -r african eyes flickr2 kelco martin mit2 platon samples
-
-((i=0))
-for d in * ; do 
-	newname="data${i}.png"
-	mv $d $newname
-	(( i++ ))
-done
-
-rm data122.png
-
-cd ../
-mv data groundTruth
+bash Prep10kWilmaBainbridge.sh
+bash PrepsomeData.sh
 
 #wget https://s3.us-east-2.amazonaws.com/iw07alexlamfacesdataset/faces96.zip
 #unzip faces96.zip
